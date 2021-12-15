ba97ac4c02fce312d9f0e9e1c5f0230a1f395781
pzhao123 <peterzhao080@gmail.com>
2021-12-15 11:27:57 -0500

    update

--- basic_server.c
+++ basic_server.c
@@ -11,9 +11,8 @@ int main() {
     from_client = server_handshake( &to_client );
     char line[BUFFER_SIZE];
 
-    while (1) {       
+    while (read(from_client, line, sizeof(line)))) {       
             
-        read(from_client, line, sizeof(line) );
         line[strlen(line) - 1] = '\0';
         if (!strcmp(line, "exit")) return 0;
         int len = strlen(line);
