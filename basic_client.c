d6d688f4b5eec140212bdccfae7f7e73fd513840
pzhao123 <peterzhao080@gmail.com>
2021-12-16 21:41:53 -0500

    update

--- basic_client.c
+++ basic_client.c
@@ -6,15 +6,17 @@ int main() {
     int to_server;
     int from_server;
     from_server = client_handshake( &to_server );
-    char line[BUFFER_SIZE];
 
     while (1) {       
+        char line[BUFFER_SIZE];
         printf("Input: ");
         fgets(line, BUFFER_SIZE, stdin);
+        line[strlen(line) - 1] = '\0';
         if (!strcmp(line, "exit")) return 0;
         write(to_server, line, sizeof(line));
-        read(from_server, line, sizeof(line));
-        printf("OUTPUT: %s\n", line);
+        char response[BUFFER_SIZE];
+        read(from_server, response, sizeof(response));
+        printf("OUTPUT: %s\n", response);
     }
     return 0;
 }
